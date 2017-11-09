1eaf50b7369d65ecd1dd5ffe6d473968bdc8fbbc
webmaster0307 <web.master0307@gmail.com>
2017-11-09 17:55:16 +0300

    Add Search result container to the main app component

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import SearchBar from '../containers/search_bar';
+import SearchResult from '../containers/search_result';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <SearchBar />
+        <SearchResult />
       </div>
     );
   }
