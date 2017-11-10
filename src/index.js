d8077d119e515c4b9a8334d09b1175aadee13f58
webmaster0307 <web.master0307@gmail.com>
2017-11-10 17:38:37 +0300

    Add shoppinglist component to a routes

--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-
 import ReduxPromise from 'redux-promise';
-
-import App from './components/app';
 import reducers from './reducers';
+import { BrowserRouter, Route } from 'react-router-dom';
+import ShoppingLists from './components/shoppinglists';
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route path="/shoppinglists" component={ShoppingLists} />
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
