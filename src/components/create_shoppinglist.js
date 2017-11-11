1512c04411c0a868b67738cf6adf54797a6a75b3
webmaster0307 <web.master0307@gmail.com>
2017-11-11 18:34:49 +0300

    Create shopping list create form elements

--- src/components/create_shoppinglist.js
+++ src/components/create_shoppinglist.js
@@ -1,13 +1,46 @@
 import React, { Component } from 'react';
-
+import { Field, reduxForm } from 'redux-form';
 class CreateShoppingList extends Component {
+    renderField(field) {
+        return (
+            <div className="form-group">
+                <label>{field.label}</label>
+                <input className="form-control" type={field.type} {...field.input} />
+            </div>
+        );
+    }
+
     render() {
         return (
             <div>
                 Create New shopping List
+                <form>
+                    <Field
+                        name="name"
+                        label="Name"
+                        type="text"
+                        component={this.renderField} />
+                    <Field
+                        name="description"
+                        label="Description"
+                        type="text"
+                        component={this.renderField} />
+                </form>
             </div>
         );
     }
 }
 
-export default CreateShoppingList;
+function validate(values) {
+    const errors = {};
+    if (!values.title || values.title.length < 3) {
+        errors.title = "Enter title that is more than three characters !";
+    }
+
+    return errors;
+}
+
+export default reduxForm({
+    validate,
+    form: 'CreateShoppingListForm'
+})(CreateShoppingList);
