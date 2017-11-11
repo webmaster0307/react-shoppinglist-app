e70caeb0ba3f5f4a526aaa8f826b0dd1937876f1
webmaster0307 <web.master0307@gmail.com>
2017-11-11 12:18:55 +0300

    Create shopping list create component

---
+++ src/components/create_shoppinglist.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CreateShoppingList extends Component {
+    render() {
+        return (
+            <div>
+                Create New shopping List
+            </div>
+        );
+    }
+}
+
+export default CreateShoppingList;
