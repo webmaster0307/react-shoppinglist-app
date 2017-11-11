21a54fb9979ea65aba93bb592fb16c70794f219d
webmaster0307 <web.master0307@gmail.com>
2017-11-11 12:17:38 +0300

    Add shopping list create compnonent route

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxPromise from 'redux-promise';
 import reducers from './reducers';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import ShoppingLists from './components/shoppinglists';
+import CreateShoppingList from './components/create_shoppinglist'
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
@@ -13,7 +14,10 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path="/shoppinglists" component={ShoppingLists} />
+        <Switch>
+          <Route path="/shoppinglists/new" component={CreateShoppingList} />
+          <Route path="/shoppinglists" component={ShoppingLists} />
+        </Switch  >
       </div>
     </BrowserRouter>
   </Provider>
