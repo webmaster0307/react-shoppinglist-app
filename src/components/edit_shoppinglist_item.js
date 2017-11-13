29671cfce6646230d0d0c588512173b86d4c84be
webmaster0307 <web.master0307@gmail.com>
2017-11-13 17:07:09 +0300

    Add default values on item edit form

--- src/components/edit_shoppinglist_item.js
+++ src/components/edit_shoppinglist_item.js
@@ -1,23 +1,34 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchListItem } from '../actions/index';
+import { fetchListItems } from '../actions/index';
 import ShoppingListItemForm from './shoppinglist_item_form';
 
 class EditShoppingListItem extends Component {
+
+    componentDidMount() {
+        const { listId } = this.props.match.params;
+        this.props.fetchListItems(listId);
+    }
 
     onSubmit(values) {
         console.log('Form has been submited:', values);
     }
 
     render() {
-
+        const { id } = this.props.match.params;
         return (
             <div>
                 Edit Item from shopping list
-                <ShoppingListItemForm initialValues={} onSubmit={this.onSubmit.bind(this)} />
+                <ShoppingListItemForm initialValues={this.props.shoppingListItems[id]} onSubmit={this.onSubmit.bind(this)} />
             </div>
         );
     }
 }
 
-export default EditShoppingListItem;
+function mapStateToProps(state) {
+    return {
+        shoppingListItems: state.shoppingListItems
+    }
+}
+
+export default connect(mapStateToProps, { fetchListItems })(EditShoppingListItem);
