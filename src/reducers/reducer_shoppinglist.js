18a6ce61293c49a080df6bff718cff9ef43ca0e9
webmaster0307 <web.master0307@gmail.com>
2017-11-13 12:39:45 +0300

    Add ShoppingListReducer to store single shopping list being edited

--- src/reducers/reducer_shoppinglist.js
+++ src/reducers/reducer_shoppinglist.js
@@ -1,4 +1,4 @@
-import { SEARCH_LISTS, FETCH_LISTS, FETCH_LIST_ITEMS, DELETE_LIST } from '../actions/index';
+import { SEARCH_LISTS, FETCH_LISTS, FETCH_LIST, FETCH_LIST_ITEMS, DELETE_LIST } from '../actions/index';
 import _ from 'lodash';
 
 export function SearchResultReducer(state = [], action) {
@@ -7,6 +7,9 @@ export function SearchResultReducer(state = [], action) {
         case SEARCH_LISTS:
             console.log('Shopping list found: ', action.payload.data);
             return action.payload.data;
+
+        default:
+            return state;
     }
 
     return state;
@@ -28,11 +31,23 @@ export function ShoppingListsReducer(state = {}, action) {
 
 }
 
+export function ShoppingListReducer(state = {}, action) {
+    console.log('action received:', action);
+    switch (action.type) {
+        case FETCH_LIST:
+            console.log('this Shopping list was found: ', action.payload.data);
+            return _.mapKeys(action.payload.data, "id");
+
+        default:
+            return state;
+    }
+}
+
 export function ShoppingListItemsReducer(state = {}, action) {
     console.log('action received:', action);
     switch (action.type) {
         case FETCH_LIST_ITEMS:
-            console.log('Shopping list found with items: ', action.payload.data);
+            console.log('Shopping list was found with details: ', action.payload.data);
             return _.mapKeys(action.payload.data, "id");
 
         default:
