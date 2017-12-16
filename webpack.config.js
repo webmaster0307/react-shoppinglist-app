02a1da0306237835f51b050b332c636f08d67fd0
webmaster0307 <web.master0307@gmail.com>
2017-12-16 18:05:04 +0300

    Add css loaders

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,17 @@ module.exports = {
       query: {
         presets: ['react', 'es2015', 'stage-1']
       }
+    },
+    {
+      test: /\.css$/,
+      loader: 'style-loader'
+    }, {
+      test: /\.css$/,
+      loader: 'css-loader',
+      query: {
+        modules: true,
+        localIdentName: '[name]__[local]___[hash:base64:5]'
+      }
     }]
   },
   resolve: {
